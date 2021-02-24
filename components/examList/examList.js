6d146e11ac540f27fd205f1701625e6a208ea4a3
Ciao-yay <1430781660@qq.com>
2021-02-24 23:47:39 +0800

    测试列表更新

--- components/examList/examList.js
+++ components/examList/examList.js
@@ -14,13 +14,20 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    show: false,
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    checkExam(e){
+      console.log(e.currentTarget.dataset)
+      this.setData({ show: true });
+    },
 
+    onClose() {
+      this.setData({ show: false });
+    },
   }
 })
